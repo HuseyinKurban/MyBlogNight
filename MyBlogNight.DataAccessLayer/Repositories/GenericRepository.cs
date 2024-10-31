d1a1da9cca9e3efc55f9bb8be4fae8594b7da150
Hüseyin Kurban <huseyinkurban03@gmail.com>
2024-10-31 21:01:13 +0300

    Add project files.

---
+++ MyBlogNight.DataAccessLayer/Repositories/GenericRepository.cs
@@ -0,0 +1,14 @@
+﻿using MyBlogNight.DataAccessLayer.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyBlogNight.DataAccessLayer.Repositories
+{
+    public class GenericRepository<T>:IGenericDal<T> where T : class
+    {
+
+    }
+}
