4bc63a8212cc26a94cd3102fabed9b9de2a84723
Hüseyin Kurban <huseyinkurban03@gmail.com>
2024-11-21 22:56:43 +0300

    Add project files.

--- MyBlogNight.PresentationLayer/Controllers/ArticleController.cs
+++ MyBlogNight.PresentationLayer/Controllers/ArticleController.cs
@@ -1,21 +1,53 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using MyBlogNight.BusinessLayer.Abstract;
+using MyBlogNight.EntityLayer.Concrete;
 
 namespace MyBlogNight.PresentationLayer.Controllers
 {
     public class ArticleController : Controller
     {
         private readonly IArticleService _articleService;
+        private readonly ICategoryService _categoryService;
 
-        public ArticleController(IArticleService articleService)
+        public ArticleController(IArticleService articleService, ICategoryService categoryService)
         {
             _articleService = articleService;
+            _categoryService = categoryService;
         }
 
         public IActionResult AricleList()
         {
             var values = _articleService.TArticleListWithCategory();
             return View(values);
+        }
+
+        [HttpGet]
+        public IActionResult CreateArticle()
+        {
+            var categoryList = _categoryService.TGetAll();
+            List<SelectListItem> values1 = (from x in categoryList
+                                            select new SelectListItem
+                                            {
+                                                Text = x.CategoryName,
+                                                Value = x.CategoryId.ToString()
+                                            }).ToList();
+            ViewBag.v1 = values1;
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult CreateArticle(Article article)
+        {
+            article.CreatedDate = DateTime.Now;
+            _articleService.TInsert(article);
+            return RedirectToAction("AricleList");
+        }
+
+        public IActionResult DeleteArticle(int id)
+        {
+            _articleService.TDelete(id);
+            return RedirectToAction("AricleList");
         }
     }
 }
